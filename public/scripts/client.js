70733a9ee971b5618840ded36573e4f066f40141
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-11-15 22:30:43 -0700

    Added footer and back to top function, added tweet css to render the tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,4 +4,23 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const MAX_CHARS = 140;
+const MAX_CHARS = 140; // MAX Letter per tweets
+
+// to click back on top appears as the user scrolls
+
+$(() => {
+
+  $(window).scroll(toggleBackToTopButton);
+  $('.back-to-top').on('click', scrollBackTop);
+
+  loadTweets();
+});
+
+
+const scrollBackTop = function() {
+  window.scroll({
+    top: 0, 
+    left: 0, 
+    behavior: 'smooth'
+  });
+};
