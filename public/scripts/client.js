38df8dfcc1d28cbc7f41b0580cc0181c79cdc035
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-11-16 12:59:13 -0700

    added scripts to render data files users and fixed the scroll to top click

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,17 +5,71 @@
  */
 
 const MAX_CHARS = 140; // MAX Letter per tweets
+let topHeader;
 
-// to click back on top appears as the user scrolls
+// to click back to the top appears as the user scrolls
 
 $(() => {
-
+  topHeader = $('.avatar').offset().top;
   $(window).scroll(toggleBackToTopButton);
   $('.back-to-top').on('click', scrollBackTop);
-
   loadTweets();
+
 });
 
+const renderTweets = (tweets) => {
+  const $tweetsContainer = $('#tweets-container');
+  $tweetsContainer.empty();
+
+  for (const tweet of tweets) {
+    $tweetsContainer.prepend(createTweetElement(tweet));
+  }
+
+};
+
+const loadTweets = () => {
+  $.get("/tweets")
+    .then((data) => {
+      renderTweets(data);
+    })    
+};
+
+const createTweetElement = (tweet) => {
+  let $tweet = (`
+  <article class="tweet">
+    <div class="header">
+      <div>
+        <img src="${tweet.user.avatars}">
+        <h3>${tweet.user.name}</h3>
+      </div>
+      <p class="handle">${tweet.user.handle}</p>
+    </div>
+    <div class="message">
+      <p>${tweet.content.text}</p>
+    </div>
+    <div class="footer"> 
+      <p class="date">${timeago.format(tweet.created_at)}</p>
+      <div class="icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </div>
+  </article>
+  `);
+
+  return $tweet;
+};
+
+
+// The button to return back to top should appear
+const toggleBackToTopButton = function() {
+  if ($(this).scrollTop() > topHeader) {
+    $('.back-to-top').show();
+  } else {
+    $('.back-to-top').hide();
+  }
+};
 
 const scrollBackTop = function() {
   window.scroll({
@@ -23,4 +77,6 @@ const scrollBackTop = function() {
     left: 0, 
     behavior: 'smooth'
   });
+  $('.new-tweet').slideDown();
+  $('#tweet-text').focus();
 };
