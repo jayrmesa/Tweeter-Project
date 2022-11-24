94b1be1a16c15eafab6d8b68e407d79d62ad8521
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-11-24 12:57:15 -0700

    Cleaned up scripts for readbility

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,7 +7,6 @@ const updateCounter = function() {
   const charLength = MAX_CHARS - $(this).val().length;
 
   const $counter = $(this).siblings().children('.counter');
-
   $counter.val(charLength);
 
   if (charLength < 0) return $counter.addClass("red");
